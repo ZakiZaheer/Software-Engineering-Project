77a391d4bf193be41dcb8b66a9ad4752c434903b
T-800-svg <shyam556124@gmail.com>
2024-11-03 04:05:17 -0500

    footer

---
+++ task_manager/lib/customWidgets/gradient_text.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+
+class GradientText extends StatelessWidget{
+  final String text;
+  final Gradient gradient;
+  final TextStyle style;
+
+  const GradientText({
+    Key?key,
+    required this.text,
+    required this.gradient,
+    this.style=const TextStyle(fontSize: 40,fontWeight: FontWeight.bold),
+  }):super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+        shaderCallback:(bounds)=>gradient.createShader(
+          Rect.fromLTWH(0, 0, bounds.width, bounds.height)
+        ),
+      child: Text(
+        text,
+        style: style.copyWith(color: Colors.white),
+      ),
+    );
+  }
+
+}
